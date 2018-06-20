9bf175a7872c85f840e6c6797ff345a9c2f65bad
Fatou12 <fatoumatadiallo12222@gmail.com>
2018-06-20 17:34:45 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -11,19 +11,19 @@ def first_student_by_index
   # Write a solution that returns the first student in the array student_names
   puts STUDENT_NAMES[0]
 end
-first_student_by_index
+#first_student_by_index
 
 def fourth_student_by_index
   puts STUDENT_NAMES[3]
   # Write a solutionputs that returns the first student in the array student_names
 end
-fourth_student_by_index
+#fourth_student_by_index
 
 def last_student_by_index
   puts STUDENT_NAMES[-1]
   # Write a solution that returns the first student in the array student_names
 end
-last_student_by_index
+#last_student_by_index
 
 def first_student_by_method
   puts STUDENT_NAMES.first
@@ -40,4 +40,4 @@ last_student_by_method
 def first_second_and_third_students
   puts STUDENT_NAMES[0..2]
 end
-first_second_and_third_students
+#first_second_and_third_students
